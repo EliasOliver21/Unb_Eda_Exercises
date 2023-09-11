685236978ee45d8c8b726386017fdba6f53098ea
elias <eliasoliveiradapaz@hotmail.com>
2023-09-11 10:56:00 -0300

    aula 03

---
+++ aula_02/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 10;
+
+    printf("Valor inteiro: %d \n", a);
+    printf("Valor Real: %f \n", 3.14159);
+    printf("Valor Real com delimitação de casa decimais: %.2f \n", 3.14159);
+    printf("Dados textuais: %c \n", 'a');
+    printf("Dados textuais: %s \n", "abacate");
+}
---
+++ aula_03/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade = 0;
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    printf("Você tem: %d anos \n", idade);
+
+}
