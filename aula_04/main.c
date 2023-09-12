62c54af8bffd9e245c7f470c93f1e1b7eee92db3
elias <eliasoliveiradapaz@hotmail.com>
2023-09-12 00:09:07 -0300

    aula 04 e 05

---
+++ aula_04/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int idade;
+    char nome[15];
+    float peso,altura,imc;
+
+    printf("Digite seu nome e idade: \n");
+    scanf("%s %d", &nome, &idade);
+
+    if (idade >= 18){
+        printf("%s você é maior de idade e ja pode tirar sua cnh.\n", nome);
+    }else{
+        printf("%s você não é maior de idade, portanto deve esperar completar 18 anos para habilitar-se.\n", nome);
+    }
+
+    printf("Digite seu peso e altura:\n");
+    scanf("%f %f", &peso, &altura);
+    imc =  peso / (altura * altura);
+    printf("Seu imc é de: %.4f. \n", imc);
+
+}
---
+++ aula_05/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int materias;
+    float media;
+
+    printf("Digite a quantidade de matérias do colegial que você considera mais gostava, e sua média geral: \n");
+    scanf("%d %f", &materias, &media);
+
+    if (materias >= 3 && media>= 6 ){
+
+        printf("Parabéns!!Você é um aluno muito aplicado.\n");
+    }else{
+
+        printf("Você poderia ser melhor.\n");
+
+    }
+
+}
