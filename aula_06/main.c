224997654600dc2f30fc609ead1ad16fb2a69796
elias <eliasoliveiradapaz@hotmail.com>
2023-09-12 21:02:20 -0300

    adicionando aula 6

---
+++ aula_06/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+    for(int i = 5;i >= 1; i--){
+
+        printf("Java is the best language. \n");
+    }
+
+    int a = 5;
+
+    while(a > 1){
+
+        printf("Em python não é usado ponto e vírgula, mas usa-se separação por indentação. \n");
+        a--;
+    }
+
+    int b = 5;
+
+    do{
+        printf("Em java utiliza-se separação por ponto e vírgula. \n");
+        b--;
+        
+    }while(b > 1 );
+
+
+}
